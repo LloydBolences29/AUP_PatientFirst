3fb01b93ad6be2099ef7e5875691113b239eb246
Isaac Alao <95632975+Isaac-ALAO@users.noreply.github.com>
2025-02-26 06:18:44 +0800

    Update Profile.jsx

--- aupcis/src/modules/Patient/Profile.jsx
+++ aupcis/src/modules/Patient/Profile.jsx
@@ -5,7 +5,7 @@ export default function Profile(){
         <nav className="NavBack">
             <ul className="NavList">
                 <li>
-                    <Link className="textdecor"><h1 className="Indi">Profile</h1></Link>
+                    <Link to="/" className="textdecor"><h1 className="Indi">Profile</h1></Link>
                 </li>
                 <div className="NavListOthers">
                     <li>
@@ -28,17 +28,29 @@ export default function Profile(){
             <div className="PersonalInfo">
                 <div className="ProfilePic"><i className="bi bi-person-circle"></i></div>
                 <h5 className="PersonalInfo2">Personal Information</h5>
-                <div className="ProfilePicEdit"><i className="bi bi-pencil-square"></i></div>
+                <div className="ProfilePicEdit"><Link to="./ProfileEdit"><i className="bi bi-pencil-square"></i></Link></div>
             </div>
         </div>
         <div>
             <ul className="ProfilePage">
-                <li>Name :</li>
-                <li>Phone :</li>
-                <li>Email Address :</li>
-                <li>Age :</li>
-                <li>Gender :</li>
-                <li>BloodType :</li>
+                <li>Name :</li><input type="text" name="" className="ProfilePageInput" />
+                <li>Phone :</li><input type="tel" name="" className="ProfilePageInput" />
+                <li>Email Address :</li><input type="email" name="" className="ProfilePageInput" />
+                <li>Age :</li><input type="date" name="" className="ProfilePageInput" />
+                <li>Gender :</li><select name="" className="ProfilePageInput">
+                    <option value="M">M</option>
+                    <option value="F">F</option>
+                </select>
+                <li>BloodType :</li><select name="" className="ProfilePageInput">
+                    <option value="A+">A+</option>
+                    <option value="A-">A-</option>
+                    <option value="B+">B+</option>
+                    <option value="B-">B-</option>
+                    <option value="O+">O+</option>
+                    <option value="O-">O-</option>
+                    <option value="AB+">AB+</option>
+                    <option value="AB-">AB-</option>
+                </select>
             </ul>
         </div>
         </>

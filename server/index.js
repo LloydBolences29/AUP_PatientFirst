b5c6bb5759748b1cb03b0f91741c18b678c1b376
LloydBolences29 <lmbolences@gmail.com>
2025-04-24 16:28:15 +0800

    another push again hehehe

--- server/index.js
+++ server/index.js
@@ -53,7 +53,7 @@ const server = http.createServer(app);
 // ✅ Middleware Setup
 app.use(cors({
   origin: ["https://aup-patient-first-frontend.vercel.app", "https://aup-patient-first.vercel.app"], // Ensure HTTP, not HTTPS
-  methods: ["GET", "POST", "PUT","OPTIONS", "PATCH"], // Explicitly allow POST requests
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"], // Allow necessary headers
   credentials: true // Allow cookies & authentication headers
 }));

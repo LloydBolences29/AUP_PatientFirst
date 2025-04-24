5fc4ae3fcf826fe3f7545d875f93430753266c19
LloydBolences29 <lmbolences@gmail.com>
2025-04-24 15:55:13 +0800

    added the log in page

--- aupcis/src/modules/Home/LogInPage.jsx
+++ aupcis/src/modules/Home/LogInPage.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../Route/AuthContext"; // Ensure correct import path
-import "./LogInPage.css";
+import "./LoginPage.css";
 
 const LogInPage = () => {
   const [isRightPanelActive, setIsRightPanelActive] = useState(false);
